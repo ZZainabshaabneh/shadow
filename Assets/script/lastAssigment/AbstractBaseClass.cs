8e0ef43ad5fac5bdfd251695693b873cf6c74159
zainab shaabneh <118971015+ZZainabshaabneh@users.noreply.github.com>
2025-04-01 18:24:57 +0300

    Add files via upload

---
+++ Assets/script/lastAssigment/AbstractBaseClass.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+namespace Assignment35
+{
+    public abstract class AbstractBaseClass : MonoBehaviour
+    {
+        public abstract void PerformAction();
+        public void PrintInfo()
+        {
+            Debug.Log("This is a concrete method in the abstract base class.");
+        }
+
+    }
+}
